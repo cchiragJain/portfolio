55cb2a10c0ae1ac892f506db317a17a9552a38a4
Chirag Jain <cjain8060@gmail.com>
2024-01-26 14:33:29 +0530

    Fixed dark mode not working due to issue in tailwind config Changed default behaviour of anchor tag to open in new window

--- components/project.tsx
+++ components/project.tsx
@@ -32,7 +32,12 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <a className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-4 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]" href={liveLink}>
+        <a
+          className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-4 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]"
+          href={liveLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 text-sm">
             {description}
